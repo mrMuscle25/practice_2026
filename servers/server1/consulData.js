1baa46f2d7deb6a4fdd3715a689171be9fa6c633
Dmitry <Diman25042005@yandex.ru>
2026-07-10 00:19:26 +0300

    Первый коммит

---
+++ servers/server1/consulData.js
@@ -0,0 +1,27 @@
+const consul = [
+     {
+          "CreateIndex": 1234567,
+          "Key": "speedometer1",
+          "Value": "120"
+     },
+     {
+          "CreateIndex": 2345678,
+          "Key": "speedometer2",
+          "Value": "130"
+     },
+     {
+          "CreateIndex": 3456789,
+          "Key": "speedometer3",
+          "Value": "15"
+     },
+     {
+          "CreateIndex": 4567890,
+          "Key": "speedometer4",
+          "Value": "16"
+     },
+     {
+          "CreateIndex": 5678901,
+          "Key": "speedometer5",
+          "Value": "80"
+     }
+]
---
+++ servers/server2/consulData.js
@@ -0,0 +1,27 @@
+const consul = [
+     {
+          "CreateIndex": 1234567,
+          "Key": "thermometer1",
+          "Value": "16°"
+     },
+     {
+          "CreateIndex": 2345678,
+          "Key": "thermometer2",
+          "Value": "21°"
+     },
+     {
+          "CreateIndex": 3456789,
+          "Key": "thermometer",
+          "Value": "11°"
+     },
+     {
+          "CreateIndex": 4567890,
+          "Key": "thermometer4",
+          "Value": "14°"
+     },
+     {
+          "CreateIndex": 5678901,
+          "Key": "thermometer5",
+          "Value": "14°"
+     }
+]
